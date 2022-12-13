244e6b420cdea1985f9971a5270489e6f64dadd9
Fran-Kee Brown <fbrowndev@gmail.com>
2022-12-13 18:59:18 -0500

    Player Movement Updated
    - Added left and right movement
    -Refined the bounce method
    - Added Color Randomization

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,16 @@ public class PlayerController : MonoBehaviour
 {
     #region Player Variables
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     [Header("Bounce Power")]
     [SerializeField] float bounceForce = 1f;
+    [SerializeField] float moveSpeed = 1f;
 
+    [Header("Player Colors")]
+    [SerializeField] Color[] color;
+
+    bool isGrounded = true;
     #endregion
 
 
@@ -20,21 +26,60 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerBounce();
+        ColorSwitcher();
+        BasicMovement();
     }
 
 
     #region Player Movement Methods
     void PlayerBounce()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void BasicMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        Vector2 Movement = Vector2.right * horizontalMovement * moveSpeed;
+
+        transform.Translate(Movement * Time.deltaTime);
+    }
+
+    #endregion
+
+    #region Collision Handlers
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    #endregion
+
+    #region Color Mechanics
+    void ColorSwitcher()
+    {
+        int RandomColor = Random.Range(0, color.Length);
+
+        //Will be added to collision handlers later
+        //Added just for testing purposes
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            sr.color = color[RandomColor];
         }
     }
 
